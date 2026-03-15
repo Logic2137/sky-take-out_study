2d8057a6df5abed47ef87072383286331c7b41bc
TimelyRain <1965795855@qq.com>
2026-03-16 00:30:50 +0800

    [day3]菜品分页查询和删除菜品

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,16 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 public interface DishService {
     public void saveWithFlavor(DishDTO dish);
+
+    PageResult pageQuery(DishPageQueryDTO dishQueryDTO);
+
+    void deleteBatch(List<Long> ids);
 }
